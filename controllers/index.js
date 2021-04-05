a9a9c4edef4cb3d444fe98e32535fab8624989b6
Hiral patel <hiralm227@gmail.com>
2021-04-05 09:56:58 -0400

    construction done

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+// we doest require to give index route after ./api/(here) its  default setting 
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
